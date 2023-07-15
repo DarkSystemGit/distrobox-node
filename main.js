a08533e074ebf10b3433be2efe408fd60973ad75
Dark System <darksystemcoder@gmail.com>
2023-07-15 17:59:30 -0400

    ability to add custom args added

--- main.js
+++ main.js
@@ -1,48 +1,74 @@
 const child_process = require("child_process");
-const distroboxFunctions ={
-    create: async(imageName,name)=>{
-        if(!(name==null)){
-            name = " -n "+name+" " 
-        }else{
-            name =" "
+const distroboxFunctions = {
+    create: async (imageName, name,args) => {
+        if (!(name == null)) {
+            name = " -n " + name + " "
+        } else {
+            name = " "
         }
-        var images={
-            ubuntu:"quay.io/toolbx-images/ubuntu-toolbox:22.04",
-            arch:"quay.io/toolbx-images/archlinux-toolbox",
-            alpine:"quay.io/toolbx-images/alpine-toolbox:3.17",
-            opensuse:"registry.opensuse.org/opensuse/distrobox:latest",
-            fedora:"registry.fedoraproject.org/fedora-toolbox:39",
-            redhat:"registry.access.redhat.com/ubi9/ubi",
-            rockyLinux:"quay.io/rockylinux/rockylinux:latest",
-            debian:"docker.io/library/debian:testing",
-            almalinux:"quay.io/almalinux/almalinux:9",
-            amazonlinux:"public.ecr.aws/amazonlinux/amazonlinux:2022.0.20220531.0",
-            void:"ghcr.io/void-linux/void-linux:latest-full-x86_64",
-            deepin:"docker.io/linuxdeepin/beige",
-            slackware:"docker.io/vbatts/slackware:14.2",
-            clearlinux:"docker.io/library/clearlinux:latest",
-            centos:"quay.io/centos/centos:7",
-            centosstream:"quay.io/toolbx-images/centos-toolbox:stream9",
-            gentoo:"docker.io/gentoo/stage3:latest",
-            kali:"docker.io/kalilinux/kali-rolling:latest",
-            mint:"docker.io/linuxmintd/mint21.1-amd64",
-            opensuse:"registry.opensuse.org/opensuse/toolbox:latest"
+        var images = {
+            ubuntu: "quay.io/toolbx-images/ubuntu-toolbox:22.04",
+            arch: "quay.io/toolbx-images/archlinux-toolbox",
+            alpine: "quay.io/toolbx-images/alpine-toolbox:3.17",
+            opensuse: "registry.opensuse.org/opensuse/distrobox:latest",
+            fedora: "registry.fedoraproject.org/fedora-toolbox:39",
+            redhat: "registry.access.redhat.com/ubi9/ubi",
+            rockyLinux: "quay.io/rockylinux/rockylinux:latest",
+            debian: "docker.io/library/debian:testing",
+            almalinux: "quay.io/almalinux/almalinux:9",
+            amazonlinux: "public.ecr.aws/amazonlinux/amazonlinux:2022.0.20220531.0",
+            void: "ghcr.io/void-linux/void-linux:latest-full-x86_64",
+            deepin: "docker.io/linuxdeepin/beige",
+            slackware: "docker.io/vbatts/slackware:14.2",
+            clearlinux: "docker.io/library/clearlinux:latest",
+            centos: "quay.io/centos/centos:7",
+            centosstream: "quay.io/toolbx-images/centos-toolbox:stream9",
+            gentoo: "docker.io/gentoo/stage3:latest",
+            kali: "docker.io/kalilinux/kali-rolling:latest",
+            mint: "docker.io/linuxmintd/mint21.1-amd64",
+            opensuse: "registry.opensuse.org/opensuse/toolbox:latest"
         }
-        var res = await runComm(`distrobox-create --root${name}-i ${images[imageName]} -Y`)
+        var res = await runComm(`distrobox-create --root${name}-i ${images[imageName]} -Y ${parseArgs(args)}`)
         return res
     },
-    remove: async (name)=>{
-        var res = await runComm(`distrobox-rm --root ${name} -Y`)
-        return res
-    }, 
-    list:async ()=>{
-        var res = await runComm(`distrobox-list --root`)
+    remove: async (name,args) => {
+        
+        var res = await runComm(`distrobox-rm --root ${name} -Y ${parseArgs(args)}`)
         return res
     },
-    stop: async (name)=>{
-        var res = await runComm(`distrobox-stop --root ${name}`)
+    list: async (args) => {
+        var res = await runComm(`distrobox-list --root ${parseArgs(args)}`)
         return res
-    }, 
+    },
+    stop: async (name,args) => {
+        var res = await runComm(`distrobox-stop --root ${name} ${parseArgs(args)}`)
+        return res
+    },
+}
+function parseArgs(args) {
+    var shorthand = {}
+    var fullArg = {}
+    var res = ""
+    try {
+        var argsKeys = Object.keys(args)
+        argsKeys.forEach(arg => {
+            if (arg.length == '0') {
+                shorthand[arg] = args[arg]
+            } else {
+                fullArg[arg] = args[arg]
+            }
+        });
+        Object.keys(shorthand).forEach(arg => {
+            res = `${res} -${arg} ${shorthand[arg]}`
+        })
+        Object.keys(fullArg).forEach(arg => {
+            res = `${res} --${arg} ${fullArg[arg]}`
+        })
+    } catch {
+        return ""
+     }
+
+    return res
 }
 async function runComm(comm) {
     return new Promise((resolve, reject) => {
